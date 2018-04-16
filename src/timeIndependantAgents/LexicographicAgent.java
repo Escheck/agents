c6075117dc6fcb5391e7dd92b10c292158d700c2
Escheck2000 <Escheck2000-o2@yahoo.de>
2018-04-16 13:25:03 +0200

    Fixed Lexicographic Agent

--- src/timeIndependantAgents/LexicographicAgent.java
+++ src/timeIndependantAgents/LexicographicAgent.java
@@ -4,12 +4,17 @@ import java.io.FileNotFoundException;
 import java.io.PrintWriter;
 import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map.Entry;
 import java.util.Random;
 import java.util.TreeMap;
+
+import com.sun.xml.bind.v2.runtime.reflect.ListIterator;
 
 import agents.SimpleAgent;
 import negotiator.Agent;
@@ -46,16 +51,17 @@ public class LexicographicAgent extends Agent {
 	private Bid lastPartnerBid;
 	private Bid lastBid; // Last sent bid 
 	private Bid actualBid; // bid to be sent next
-	private TreeMap<Double, List<Bid>> listOfAllBids; // TreeMap die als Key die Utility der bids hat und als Value eine Liste mit allen Bids die genau die utility haben
+	private List<Bid> listOfAllBids; // Liste die alle Bids enthaelt. Wird Sortiert, darum muessen die Offers der Liste nur noch noch der Reihe nach durchgegangen werden
 	private TreeMap<Integer, Double> listOfWeightedIssues;
 	private AdditiveUtilitySpace castedAdditiveUtilitySpace;
 	private List<Bid> sentOffers;
 	private List<Bid> allBidsList;
-	
+	private List<Issue> sortByWeightIssueList;
+	private Integer ArrayPositionNextOffer;
 	//Delimiter used in CSV file
-    private static final String COMMA_DELIMITER = ",";
+	private static final String COMMA_DELIMITER = ",";
 
-    //CSV file header
+	//CSV file header
 	private String FILE_HEADER = "Agent,";
 	/**
 	 * init is called when a next session starts with the same opponent.
@@ -66,16 +72,16 @@ public class LexicographicAgent extends Agent {
 			lastBid = null;
 			actualBid = null;
 			lastPartnerBid = null;
-			listOfAllBids = new TreeMap<Double, List<Bid>>();
 			listOfWeightedIssues = new TreeMap<Integer, Double>();
 			castedAdditiveUtilitySpace = (AdditiveUtilitySpace) utilitySpace;
 			sentOffers = new ArrayList<Bid>();
 			allBidsList = new ArrayList<Bid>();
-			
+			sortByWeightIssueList = new ArrayList<Issue>();
 			getAllBidsList();
 			printAllBids(allBidsList, "AllBids_"+getAgentID()+getName());
-			getAllBids();
-			
+			getOrderedIssueList();
+			ArrayPositionNextOffer = 0;
+			listOfAllBids = getAllBids();
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -107,50 +113,30 @@ public class LexicographicAgent extends Agent {
 		try {
 			if (lastBid == null){
 				// First round. place our best bid.
-				actualBid = utilitySpace.getMaxUtilityBid();
+				actualBid = listOfAllBids.get(ArrayPositionNextOffer);
+				ArrayPositionNextOffer++;
 				action = new Offer(getAgentID(), actualBid);
 				lastBid = actualBid;
 				sentOffers.add(actualBid);
-				listOfAllBids.remove(getWeightedUtiliy(actualBid));
+				
 				//listOfAllBids.get(getUtil(actualBid)).remove(actualBid);
 			}else if (actionOfPartner instanceof Offer) {
-				double lastBidUtil = getWeightedUtiliy(lastBid);
-				boolean foundBid = false;
-				while(listOfAllBids.floorEntry(lastBidUtil) != null && !foundBid) {
-					
-					Entry<Double, List<Bid>> actualUtilEntryList = listOfAllBids.floorEntry(lastBidUtil);
-					for (Bid b: actualUtilEntryList.getValue()) {
-						int differences = 0;
-						Iterator<HashMap.Entry<Integer, Value>> iterator = b.getValues().entrySet().iterator();
-						
-						while (iterator.hasNext() && differences < 2) {
-							HashMap.Entry<Integer, Value> entry = iterator.next();
-							if(!entry.getValue().equals(lastBid.getValue(entry.getKey()))){
-								differences++;
-							}
-						}
-						if(differences==1){
-							// Der Bid mit der höchsten Utility und einer unterschiedlichen Issue wurde gefunden
-							actualBid = b;
-							listOfAllBids.remove(getWeightedUtiliy(actualBid));
-							foundBid=true;
-							break;
-						} 
-						//System.out.println("Key = " + entry.getKey() + ", Value = " + entry.getValue()); 		
-					}
-					//Alle Bids mit der gleichen Utility durchgegangen => Utility verringern
-					lastBidUtil = listOfAllBids.lowerKey(lastBidUtil);
+				double utilNextBid = getUtil(listOfAllBids.get(ArrayPositionNextOffer));
+				double utilPrevBid = getUtil(listOfAllBids.get(ArrayPositionNextOffer-1));
+				while(utilNextBid > utilPrevBid){
+					ArrayPositionNextOffer++;
+					utilNextBid = getUtil(listOfAllBids.get(ArrayPositionNextOffer));
 				}
-
-				//System.out.println(getAgentID()+ " " +getUtil(actualBid));
-
+				actualBid = listOfAllBids.get(ArrayPositionNextOffer);
+				ArrayPositionNextOffer++;
+				action = new Offer(getAgentID(), actualBid);
 				//Check if our next offer is lower than our reservation level
 				if(checkReject(actualBid)){
 					action = new EndNegotiation(getAgentID());
 					//Check if Opponent offer is acceptable
 				}else if(checkAcceptance(lastPartnerBid)){
 					action = new Accept(getAgentID(), lastBid);
-					
+
 				}else{
 					lastBid = actualBid;				
 					sentOffers.add(actualBid);
@@ -159,7 +145,8 @@ public class LexicographicAgent extends Agent {
 			}else if (actionOfPartner instanceof Reject){
 				//What should happen here?
 			}
-		this.printAllBids(sentOffers, getAgentID().toString()+getName());
+			this.printAllBids(sentOffers, getAgentID().toString()+getName());
+			
 		} catch (Exception e) {
 			System.out.println("Exception in ChooseAction:" + e.getMessage());
 			if (lastPartnerBid != null) {
@@ -170,46 +157,61 @@ public class LexicographicAgent extends Agent {
 		}
 		return action;
 	}
-
-	private void getAllBids() throws Exception {
+	
+	private List<Bid> getAllBids() throws Exception {
+		BidIterator iterator = new BidIterator(utilitySpace.getDomain());
+		List<Bid> bidList = new ArrayList<Bid>();
+		while (iterator.hasNext()) {
+			Bid tempBid = iterator.next();
+			bidList.add(tempBid);
+		}
+		orderLexico(bidList);
+		return bidList;
+	}
+	
+	private void getOrderedIssueList() throws Exception {
 		//Additiver utilitspace macht nichts anderes als issue.getWeight*issue.getValue() für jede Issue und summiert das auf.
 		//Ich aendere das weight und multipliziere es mit Integer.MAX_VALUE. Damit sollte die Utility in erster Linie von der Issue mit den hoechsten weight abhaengen, 
 		//da dieser Wert durch die multiplikation so gross wird dass alle anderen irrelevant sind
 		//Vorgehen aus: Internet Multiattribute Group Decision Support in Electronic Commerce - Beroggi (2003), p 484
-		
+
 		//UtilitySpace und issues holen
 		List<Issue> all_Issues= utilitySpace.getDomain().getIssues();	
-		
+
 		//Das Weight pro Issue holen
 		for (Issue issue : all_Issues){
 			double weight = castedAdditiveUtilitySpace.getWeight(issue.getNumber());
 			listOfWeightedIssues.put(issue.getNumber(), weight);
+			sortByWeightIssueList.add(issue);
 			//System.out.println("Issue: "+issue + ", weight" + weight);
 		}
-		
-		//Alle Weights der Issues mit Integer.MAX_VALUE multiplizieren und in die getAllBids TreeMap speichern. Dann kann in der Methode chooseAction die TreeMap wieder verwendet werden
-		//wie in den anderen Agents auch
-				
-		BidIterator iterator = new BidIterator(utilitySpace.getDomain());
-		while (iterator.hasNext()) {
-			Bid tempBid = iterator.next();
-			List<Bid> bidList = new ArrayList<Bid>(); //bidList enhaelt alle Bids mit der gleichen utility	
-			//Ab hier unterscheidet sich der LexicographicAgent. getUtil wird nicht aufgerufen sondern selber berechnet. 
-			bidList.add(tempBid);
-			//TODO TreeMap in Liste umwandeln? Reihenfolge ist durch die sortierung gegeben dann einfach von oben nach unten durch laufen.
-			listOfAllBids.put(getWeightedUtiliy(tempBid), bidList);
-		}	
-	}
 
+		Collections.sort(sortByWeightIssueList , new Comparator<Issue>() {
+			@Override
+			public int compare(Issue issue1, Issue issue2)
+			{	
+				return new Double(castedAdditiveUtilitySpace.getWeight(issue2.getNumber())).compareTo(castedAdditiveUtilitySpace.getWeight(issue1.getNumber()));
+			}
+		});
+	}
 	
-	private double getWeightedUtiliy(Bid bid){
-		double util = 0;	
-		double issueUtil = 0;
-		for(Issue issue: bid.getIssues()){
-			issueUtil = castedAdditiveUtilitySpace.getEvaluation(issue.getNumber(), bid);	
-			util += issueUtil * listOfWeightedIssues.get(issue.getNumber()) * Integer.MAX_VALUE;
-		}
-		return util;
+	private void orderLexico(List<Bid> list){
+		Collections.sort(list, new Comparator<Bid>() {
+			@Override
+			public int compare(Bid bid1, Bid bid2)
+			{	
+				for (Issue issue: sortByWeightIssueList){
+					double valueIssue1 = castedAdditiveUtilitySpace.getEvaluation(issue.getNumber(), bid1);
+					double valueIssue2 = castedAdditiveUtilitySpace.getEvaluation(issue.getNumber(), bid2);
+					int compareValue = new Double(valueIssue2).compareTo(valueIssue1);				        	
+					if (compareValue != 0) {
+						return compareValue; 
+					}
+				}
+				//Beide bids sind identisch. Sollte niemals rauskommen da irgendwann in der for Schleife ein Unterschied sein muss. 
+				return 0;
+			}
+		});
 	}
 	
 	private Double getUtil(Bid bid) throws Exception {
@@ -234,21 +236,21 @@ public class LexicographicAgent extends Agent {
 	}
 
 	private void printAllBids(List<Bid> bids, String filename) throws FileNotFoundException, UnsupportedEncodingException{
-//		for(Entry<Double, List<Bid>> entry : listOfAllBids.entrySet()) {
-//			for(Bid b: entry.getValue()){
-//				System.out.println(getAgentID() + " Bid: " +b.toString() + " util: " + entry.getKey());		
-//			}
-//		}
+		//		for(Entry<Double, List<Bid>> entry : listOfAllBids.entrySet()) {
+		//			for(Bid b: entry.getValue()){
+		//				System.out.println(getAgentID() + " Bid: " +b.toString() + " util: " + entry.getKey());		
+		//			}
+		//		}
 		PrintWriter writer = new PrintWriter("C:\\Users\\Matthes\\Downloads\\" + filename + ".csv", "UTF-8");
 		FILE_HEADER = "Agent,";
-		
+
 		for (Issue issue: utilitySpace.getDomain().getIssues()){
 			double weight = castedAdditiveUtilitySpace.getWeight(issue.getNumber());
 			FILE_HEADER = FILE_HEADER + issue +"(Weight: " + weight + " )"+ COMMA_DELIMITER + issue+"-Utility"+ COMMA_DELIMITER;
 		}
 		FILE_HEADER = FILE_HEADER + "Utility";
 		writer.println(FILE_HEADER);
-		
+
 		for(Bid tempBid: bids){
 			String line = getAgentID() + COMMA_DELIMITER;
 			for (Entry<Integer, Value> entry: tempBid.getValues().entrySet()){
@@ -263,7 +265,7 @@ public class LexicographicAgent extends Agent {
 	private void getAllBidsList(){
 		BidIterator iterator = new BidIterator(utilitySpace.getDomain());
 		while(iterator.hasNext()){
-		allBidsList.add(iterator.next());
+			allBidsList.add(iterator.next());
 		}
 	}
 
